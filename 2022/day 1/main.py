da2ea9f6a6eab79df24632a6092b8f6239e6cd27
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-12-02 15:45:26 +0100

    Update main.py

--- 2022/day 1/main.py
+++ 2022/day 1/main.py
@@ -14,19 +14,24 @@ with open('elves.txt') as f:
 
 df = pandas.DataFrame(calories_per_elves, columns=["Calories"])
 
-top_1 = df['Calories'].max()
-print(f"The Elf with the most calories is carrying {top_1} calories.\n")
 
-top_3 = df.nlargest(3, 'Calories')
-top_3_sum = top_3.sum().values[0]
-print(f"The top 3 Elves are carrying a total of {top_3_sum} calories.")
+if __name__ == '__main__':
+    top_1 = df['Calories'].max()
+    print(f"The Elf with the most calories is carrying {top_1} calories.\n")
+
+    top_3 = df.nlargest(3, 'Calories')
+    top_3_sum = top_3.sum().values[0]
+    print(f"The top 3 Elves are carrying a total of {top_3_sum} calories.")
 
 
 #Naming the index column
 #df.index.name = "Elf index" #https://stackoverflow.com/a/37968994
 
 #starting index at 1
-#df.index += 1 
+#df.index += 1
 
 #Displaying the entire Series with the most calories
 # most_calories = df[df['Calories']==df['Calories'].max()]
+
+
+
